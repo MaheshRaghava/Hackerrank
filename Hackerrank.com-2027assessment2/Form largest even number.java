64999f8a11c4eeeeaf05b9b38b259b9df27a1734
Mahesh Raghava <160461240+MaheshRaghava@users.noreply.github.com>
2025-03-11 22:09:23 +0530

    Update Form largest even number.java

--- Hackerrank.com-2027assessment2/Form largest even number.java
+++ Hackerrank.com-2027assessment2/Form largest even number.java
@@ -1,15 +1,17 @@
 import java.util.*;
-
-public class LargestEvenNumber {
-    public static void main(String[] args) {
+public class LargestEvenNumber
+{
+    public static void main(String[] args)
+    {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();  
-        
         Set<Character> uniqueDigits = new HashSet<>();
         List<Character> digits = new ArrayList<>();
         
-        for (char ch : input.toCharArray()) {
-            if (Character.isDigit(ch)) {
+        for (char ch : input.toCharArray())
+        {
+            if (Character.isDigit(ch))
+            {
                 uniqueDigits.add(ch);
             }
         }
@@ -18,20 +20,24 @@ public class LargestEvenNumber {
         Collections.sort(digits, Collections.reverseOrder());  
         
         Character evenDigit = null;
-        for (int i = digits.size() - 1; i >= 0; i--) {
-            if ((digits.get(i) - '0') % 2 == 0) {
+        for (int i = digits.size() - 1; i >= 0; i--) 
+        {
+            if ((digits.get(i) - '0') % 2 == 0) 
+            {
                 evenDigit = digits.remove(i);
                 break;
             }
         }
         
-        if (evenDigit == null) {
+        if (evenDigit == null)
+        {
             System.out.println("-1");
             return;
         }
         
         StringBuilder result = new StringBuilder();
-        for (char d : digits) {
+        for (char d : digits) 
+        {
             result.append(d);
         }
         result.append(evenDigit);  
