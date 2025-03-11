910be50a3e9f83ae9a1076d06e6f15134524c340
Mahesh Raghava <160461240+MaheshRaghava@users.noreply.github.com>
2025-03-11 21:56:10 +0530

    Update OTP with odd squares.java

--- Hackerrank.com-2027assessment2/OTP with odd squares.java
+++ Hackerrank.com-2027assessment2/OTP with odd squares.java
@@ -1,45 +1,27 @@
 import java.util.*;
-
-public class OTPGenerator {
+public class OTPGenerator
+{
     public static void main(String[] args)
-    
     {
         Scanner sc = new Scanner(System.in);
-
         String[] input = sc.nextLine().split(" ");
-       
-        
         StringBuilder otpString = new StringBuilder();
-
         for (String num : input)
-        
         {
             int n = Integer.parseInt(num);
-            
-            
             if (n % 2 != 0)
-            
-            
              {
                 otpString.append(n * n);
              }
         }
-
         if (otpString.length() >= 4) 
-        
-        
         {
             System.out.println(otpString.substring(0, 4));
         }
-        
-        
         else 
-        
-        
-        {
+        {a
             System.out.println("-1");
         }
-
         sc.close();
     }
 }
