927317bdddb7dc7094bf512352d2ae85ad0f8b32
Mahesh Raghava <160461240+MaheshRaghava@users.noreply.github.com>
2025-03-11 22:18:23 +0530

    Update Sort the Dates.java

--- Hackerrank.com-2027assessment2/Sort the Dates.java
+++ Hackerrank.com-2027assessment2/Sort the Dates.java
@@ -1,1 +1,29 @@
-hii
+import java.util.*;
+
+public class SortDates {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        
+        String[] dateStrings = input.split(", ");
+        List<Date> dates = new ArrayList<>();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy", Locale.ENGLISH);
+
+        try {
+            for (String dateStr : dateStrings) {
+                dates.add(sdf.parse(dateStr));
+            }
+            
+            Collections.sort(dates);
+            
+            List<String> sortedDates = new ArrayList<>();
+            for (Date date : dates) {
+                sortedDates.add(sdf.format(date));
+            }
+            
+            System.out.println(String.join(", ", sortedDates));
+        } catch (Exception e) {
+            System.out.println("Invalid date format!");
+        }
+    }
+}
