3731c78b6361aad8dd7af7119045a8a62ed1a377
Mahesh Raghava <160461240+MaheshRaghava@users.noreply.github.com>
2025-03-11 17:53:26 +0530

    Update Minimum Profit_WIPRO.java

--- Hackerrank.com-27batch1/Minimum Profit_WIPRO.java
+++ Hackerrank.com-27batch1/Minimum Profit_WIPRO.java
@@ -1,10 +1,9 @@
 import java.io.*;
 import java.util.*;
-
-public class Solution {
-
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+public class Solution
+    {
+    public static void main(String[] args) 
+        {
          Scanner sc = new Scanner(System.in);
         int m = sc.nextInt();
         int n = sc.nextInt();
@@ -12,16 +11,16 @@ public class Solution {
         String s = String.valueOf(m);
         StringBuilder numStr = new StringBuilder();
         
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) 
+        {
             numStr.append(s);
         }
         
         StringBuilder res = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++)
+        {
             res.append(numStr.charAt(i + (n - 1)));
         }
-        
         System.out.println(Integer.parseInt(res.toString()));
-        sc.close();
     }
 }
