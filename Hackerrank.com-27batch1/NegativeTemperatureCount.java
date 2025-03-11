43247d443b74ce1a7a584edaacf61fe0aeab2aa7
Mahesh Raghava <160461240+MaheshRaghava@users.noreply.github.com>
2025-03-11 16:35:01 +0530

    Update NegativeTemperatureCount.java

--- Hackerrank.com-27batch1/NegativeTemperatureCount.java
+++ Hackerrank.com-27batch1/NegativeTemperatureCount.java
@@ -1,25 +1,19 @@
-import java.util.Scanner;  // Import Scanner class for user input
-
-public class NegativeTemperatureCount {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);  // Create Scanner object for input
-
-        // Read the number of products
-        int numOfProducts = scanner.nextInt();  // Read N (number of products)
-
-        int count = 0; // Counter to keep track of negative temperatures
-
-        // Read temperatures and count how many are negative
-        for (int i = 0; i < numOfProducts; i++) {
-            int temp = scanner.nextInt();  // Read temperature
-            if (temp < 0) {  // Check if it is negative
-                count++;  // Increment counter
+import java.util.Scanner; 
+public class NegativeTemperatureCount
+    {
+    public static void main(String[] args)
+        {
+        Scanner sc = new Scanner(System.in); 
+        int n = sc.nextInt();  
+        int count = 0; 
+        for (int i=0; i<n; i++) 
+        {
+            int temp = sc.nextInt(); 
+            if (temp < 0)
+            {  
+                count++;
             }
         }
-
-        // Print the final count of negative temperature products
         System.out.println(count);
-
-        scanner.close();  // Close the scanner
     }
 }
