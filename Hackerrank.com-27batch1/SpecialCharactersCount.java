4472e7d1a55655b00cc4f0b0c7a52da79e5796b0
Mahesh Raghava <160461240+MaheshRaghava@users.noreply.github.com>
2025-03-11 16:33:05 +0530

    Update SpecialCharactersCount.java

--- Hackerrank.com-27batch1/SpecialCharactersCount.java
+++ Hackerrank.com-27batch1/SpecialCharactersCount.java
@@ -1,26 +1,23 @@
 import java.util.Scanner;
 
 public class MissesCounter {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the input string:");
         String input = scanner.nextLine();
         scanner.close();
-
-        // Define the characters to be counted as misses
         char[] misses = {'@', '$', '^'};
         int count = 0;
-
-        // Count occurrences of misses in the input string
-        for (char c : input.toCharArray()) {
-            for (char miss : misses) {
-                if (c == miss) {
+        for (char c : input.toCharArray())
+        {
+            for (char miss : misses)
+            {
+                if (c == miss)
+                {
                     count++;
                 }
             }
         }
-
-        // Print the result
         System.out.println(count);
     }
 }
