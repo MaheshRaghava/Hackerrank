e9cf712f2d7931b0e681ac669bde07a7853036da
Mahesh Raghava <160461240+MaheshRaghava@users.noreply.github.com>
2025-03-11 21:37:14 +0530

    Update Determine Uniqueness rank.java

--- Hackerrank.com-2027assessment2/Determine Uniqueness rank.java
+++ Hackerrank.com-2027assessment2/Determine Uniqueness rank.java
@@ -1,1 +1,43 @@
-hii
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class UniquenessRank {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Read input values
+        int n = scanner.nextInt(); // Read N
+        int z = scanner.nextInt(); // Read Z
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt(); // Read array elements
+        }
+
+        // Check uniqueness
+        Set<Integer> uniqueElements = new HashSet<>();
+        for (int num : arr) {
+            uniqueElements.add(num);
+        }
+        int uniqueCount = uniqueElements.size();
+
+        if (uniqueCount == n) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+        // Determine the uniqueness rank
+        if (uniqueCount == z) {
+            System.out.println("Good");
+        } else if (uniqueCount < z) {
+            System.out.println("Bad");
+        } else {
+            System.out.println("Average");
+        }
+
+        scanner.close();
+    }
+}
