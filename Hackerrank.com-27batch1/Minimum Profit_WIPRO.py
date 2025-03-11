e74cae68cf88126958b4bdc8cd3e12410cc11d05
Mahesh Raghava <160461240+MaheshRaghava@users.noreply.github.com>
2025-03-11 17:08:34 +0530

    Update Minimum Profit_WIPRO.py

--- Hackerrank.com-27batch1/Minimum Profit_WIPRO.py
+++ Hackerrank.com-27batch1/Minimum Profit_WIPRO.py
@@ -1,1 +1,20 @@
-hii
+def find_min_profits(itemsSold, noOfDays, profits):
+  
+
+  min_profits = []
+  for day in range(noOfDays):
+    min_profit = profits[0][day]  
+    for item in range(1, itemsSold):
+      if profits[item][day] < min_profit:
+        min_profit = profits[item][day]
+    min_profits.append(min_profit)
+  return min_profits
+
+itemsSold, noOfDays = map(int, input().split())
+
+profits = []
+for _ in range(itemsSold):
+  profits.append(list(map(int, input().split())))
+
+result = find_min_profits(itemsSold, noOfDays, profits)
+print(*result)
