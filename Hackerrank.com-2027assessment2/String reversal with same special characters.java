1deb8bce68de1aee964046de57c939fbf0e254b7
Mahesh Raghava <160461240+MaheshRaghava@users.noreply.github.com>
2025-03-11 22:11:24 +0530

    Update String reversal with same special characters.java

--- Hackerrank.com-2027assessment2/String reversal with same special characters.java
+++ Hackerrank.com-2027assessment2/String reversal with same special characters.java
@@ -1,1 +1,28 @@
-hii
+import java.util.*;
+
+public class ReverseAlphanumeric {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        
+        char[] chars = input.toCharArray();
+        List<Character> alphanumeric = new ArrayList<>();
+        
+        for (char ch : chars) {
+            if (Character.isLetterOrDigit(ch)) {
+                alphanumeric.add(ch);
+            }
+        }
+        
+        Collections.reverse(alphanumeric);
+        
+        int index = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (Character.isLetterOrDigit(chars[i])) {
+                chars[i] = alphanumeric.get(index++);
+            }
+        }
+        
+        System.out.println(new String(chars));
+    }
+}
