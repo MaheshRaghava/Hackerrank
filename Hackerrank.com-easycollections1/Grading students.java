f8c1aad90a8442fc01b3763729b97e71069304e0
Mahesh Raghava <160461240+MaheshRaghava@users.noreply.github.com>
2025-03-14 21:51:51 +0530

    Update Grading students.java

--- Hackerrank.com-easycollections1/Grading students.java
+++ Hackerrank.com-easycollections1/Grading students.java
@@ -10,7 +10,7 @@ import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
-class Solution
+class Result
 {
 
     public static List<Integer> gradingStudents(List<Integer> grades)
